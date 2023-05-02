f91ebbd853982c4965ea78e5fd106aa6e20b9345
麻生大翔 <hiroto.asou@M443-sakai.local>
2023-05-02 17:27:42 +0900

    英語表記を日本語表記に修正

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,97 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Card.css"
+import { getPokemonAbility, getPokemonName, getPokemonType } from "../utils/pokemon";
 
 const Card = ({ pokemon }) => {
+
+
+  // stateの宣言
+  const [pokemonTypeURL, setPokemonTypeURL] = useState([]);
+  const [pokemonName, setPokemonName] = useState([]);
+  const [pokemonAbility, setPokemonAbility] = useState([]);
+
+// ポケモンのタイプを日本語として出力する関数　↓
+  let resPokemonTypes = pokemon.types.map((v) => {
+    let typesURL = v.type.url;
+    return typesURL;
+  });
+
+  const loadPokemonType = async (data) => {
+    let _pokemonType = await Promise.all(
+      data.map(async (pokemon) => {
+        let pokemonTypeDetail = await getPokemonType(pokemon);
+        let jaName = pokemonTypeDetail.names.find(name => name.language.name === "ja").name;
+        return jaName;
+      })
+    );
+    let joinedTypes = _pokemonType.join(" / ");
+    setPokemonTypeURL(joinedTypes);
+  };
+// ポケモンのタイプを日本語として出力する関数　↑
+// ポケモンのタイプを日本語として出力する関数　↓
+let resPokemonAbility = pokemon.abilities.map((v) => {
+  let AbilityURL = v.ability.url;
+  return AbilityURL;
+});
+
+const loadPokemonAbility = async (data) => {
+  let _pokemonAbility = await Promise.all(
+    data.map(async (pokemon) => {
+      let pokemonAbilityDetail = await getPokemonAbility(pokemon);
+      let jaName = pokemonAbilityDetail.names.find(name => name.language.name === "ja").name;
+      return jaName;
+    })
+  );
+  let joinedAbilitys = _pokemonAbility.join(" / ");
+  setPokemonAbility(joinedAbilitys);
+};
+// ポケモンのタイプを日本語として出力する関数　↑
+// ポケモンの名前を日本語として出力する関数　↓
+let pokemonNameDetail = pokemon.species.url;
+
+const loadPokemonName = async (data) => {
+  let response = await fetch(data);
+  let result = await response.json();
+  let jaName = result.names.find(name => name.language.name === "ja").name;
+  setPokemonName(jaName);
+};
+// ポケモンの名前を日本語として出力する関数　↑
+
+
+  useEffect(() => {
+    loadPokemonType(resPokemonTypes);
+    loadPokemonName(pokemonNameDetail);
+    loadPokemonAbility(resPokemonAbility);
+  },[])
+
+  // コンソールで確認するエリア
+  console.log(pokemon);
+  
+
   return (
     <div className="card">
       <div className="cardImg">
-        <img src={pokemon.sprites.front_default} alt="" />
+        <img src={pokemon.sprites.other['official-artwork'].front_default} alt="" />
       </div>
-      <h3 className="cardNames">{pokemon.name}</h3>
+      <h3 className="cardNames">{pokemonName}</h3>
       <div className="cardTypes">
         <span>タイプ：</span>
-        {pokemon.types.map((type) => {
+        {/* {pokemon.types.map((v) => {
           return (
-              <span key={type.type.name}> {type.type.name} /</span>
+              <span key={v.type.name}> {v.type.name} /</span>
           );
-        })}
+        })} */}
+        {pokemonTypeURL}
       </div>
       <div className="cardInfo">
         <div className="cardData">
-          <p>重さ：{pokemon.weight}</p>
+          <p>重さ：{pokemon.weight / 10} kg</p>
         </div>
         <div className="cardData">
-          <p>高さ：{pokemon.height}</p>
+          <p>高さ：{pokemon.height / 10} m</p>
         </div>
-        <div className="cardData">
-          <p>能力：{pokemon.abilities[0].ability.name}</p>
+        <div className="cardData cardAbility">
+          <p>能力：{pokemonAbility}</p>
         </div>
       </div>
     </div>
